719c11e171cc63f19ad5649d02b0141685b21a8c
antonio <antg3102@gmail.com>
2025-02-02 23:05:36 -0300

    Projeto Java

--- src/ProjetoJava/projetoCalculoImposto.java
+++ src/ProjetoJava/projetoCalculoImposto.java
@@ -2,16 +2,18 @@ package ProjetoJava;
 
 public class projetoCalculoImposto {
     public static <string> void main(String[] args) {
-        double salario = 90000;
-        double imposto1 = 9.70 / 100;
-        double imposto2 = 37.35 / 100;
-        double imposto3 = 45.05 / 100;
-        double valorDoImposto;
-        if (salario <= 34714) {
+        double salario = 1518;
+        double imposto1 = 7.5 / 100;
+        double imposto2 = 15 / 100;
+        double imposto3 = 22.5 / 100;
+        double valorDoImposto = 0;
+        if (salario >= 2259) {
             valorDoImposto = salario * imposto1;
-        } else if (salario >= 34714 && salario <= 66507) {
+        } else if (salario >= 2826 && salario <= 3751) {
             valorDoImposto = salario * imposto2;
-        } else {
+        } else if (salario < 2259) {
+            System.out.println("voce esta insento de declara o imposto de renda");
+        }else {
             valorDoImposto = salario * imposto3;
         }
 

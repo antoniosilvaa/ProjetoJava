d94cc77ea829380c65da00a074572d2e47fb0bf9
antonio <antg3102@gmail.com>
2025-02-03 19:02:48 -0300

    Projeto Java

---
+++ src/ProjetoJava/EstruturasDeRepetição.java
@@ -0,0 +1,10 @@
+package ProjetoJava;
+
+public class EstruturasDeRepetição {
+    public static <string> void main(String[] args) {
+        for (int i = 3; i <= 1000  ; i++){
+            if(i % 2 == 0 )
+            System.out.println(i);
+        }
+    }
+}

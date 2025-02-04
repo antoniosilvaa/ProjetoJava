57903a36a4982e1a506195e9f1d0555e4823d3e2
antonio <antg3102@gmail.com>
2025-02-04 15:09:25 -0300

    Projeto Java

--- src/ProjetoJava/ProjetoDeParcelaDoCarro.java
+++ src/ProjetoJava/ProjetoDeParcelaDoCarro.java
@@ -3,11 +3,11 @@ package ProjetoJava;
 public class ProjetoDeParcelaDoCarro {
     public static void main(String[] args) {
         int valorDaParcela;
-        int valorDoCarro = 12000;
+        int valorDoCarro = 40000;
         for (int parcela = 1; parcela <= valorDoCarro; parcela++){
             valorDaParcela =   valorDoCarro / parcela;
-            if(valorDaParcela >= 700){
-               if (parcela > 12){
+            if(valorDaParcela >= 0){
+               if (parcela >50){
                    break;
                }
                 System.out.println("Parcela " + parcela + " R$" + valorDaParcela);

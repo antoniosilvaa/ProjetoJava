4e74fc19c2ed13e51fe26c63e171a0a8fac7853d
antonio <antg3102@gmail.com>
2025-02-04 18:48:29 -0300

    Projeto Java

--- src/ProjetoJava/ProjetoDeParcelaDoCarro.java
+++ src/ProjetoJava/ProjetoDeParcelaDoCarro.java
@@ -2,8 +2,8 @@ package ProjetoJava;
 
 public class ProjetoDeParcelaDoCarro {
     public static void main(String[] args) {
-        int valorDaParcela;
-        int valorDoCarro = 40000;
+        float valorDaParcela;
+        float valorDoCarro = 40000;
         for (int parcela = 1; parcela <= valorDoCarro; parcela++){
             valorDaParcela =   valorDoCarro / parcela;
             if(valorDaParcela >= 0){
---
+++ src/ProjetoJava/arrays.java
@@ -0,0 +1,18 @@
+package ProjetoJava;
+
+public class arrays {
+    public static void main(String[] args) {
+        String[] nomes = new String[3];
+        nomes[0] = "antonio";
+        nomes[1] = "aride";
+        nomes[2] = "bia";
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(nomes[i]);
+
+        }
+
+
+
+    }
+}

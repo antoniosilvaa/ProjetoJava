0e90451158b364482fe509ab963582f619005573
antonio <antg3102@gmail.com>
2025-02-05 15:02:46 -0300

    Projeto Java

--- src/ProjetoJava/arrays.java
+++ src/ProjetoJava/arrays.java
@@ -3,9 +3,9 @@ package ProjetoJava;
 public class arrays {
     public static void main(String[] args) {
         String[] nomes = new String[3];
-        nomes[0] = "antonio";
-        nomes[1] = "aride";
-        nomes[2] = "bia";
+        nomes[0] = "Antonio";
+        nomes[1] = "Aride";
+        nomes[2] = "Bia";
 
         for (int i = 0; i < 3; i++) {
             System.out.println(nomes[i]);

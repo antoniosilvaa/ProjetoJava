760215261d63aa4e94a7329913fd971ac40c165a
antonio <antg3102@gmail.com>
2025-02-06 12:08:26 -0300

    Projeto Java

---
+++ src/introduçãoClasses/dominio/ProjetoCarro.java
@@ -0,0 +1,8 @@
+package introduçãoClasses.dominio;
+
+public class ProjetoCarro {
+    public String nome;
+    public String modelo;
+    public int ano;
+    public String cor;
+}
---
+++ src/introduçãoClasses/teste/CarroTeste.java
@@ -0,0 +1,32 @@
+package introduçãoClasses.teste;
+
+import introduçãoClasses.dominio.ProjetoCarro;
+
+public class CarroTeste {
+    public static void main(String[] args) {
+        ProjetoCarro projetoCarro = new ProjetoCarro();
+        projetoCarro.nome = "Volkswagen";
+        projetoCarro.modelo = "Gol";
+        projetoCarro.ano = 2016;
+        projetoCarro.cor = "Prata";
+
+        System.out.println("Marca: " + projetoCarro.nome);
+        System.out.println("Modelo " + projetoCarro.modelo);
+        System.out.println("Ano: " + projetoCarro.ano);
+        System.out.println("Cor: " + projetoCarro.cor);
+
+        System.out.println("\n---------------");
+
+        ProjetoCarro projetoCarro2 = new ProjetoCarro();
+        projetoCarro2.nome = "Fiat";
+        projetoCarro2.modelo = "Uno";
+        projetoCarro2.ano = 2024;
+        projetoCarro2.cor = "Azul";
+
+        System.out.println("Marca: " + projetoCarro2.nome);
+        System.out.println("Modelo " + projetoCarro2.modelo);
+        System.out.println("Ano: " + projetoCarro2.ano);
+        System.out.println("Cor: " + projetoCarro2.cor);
+
+    }
+}

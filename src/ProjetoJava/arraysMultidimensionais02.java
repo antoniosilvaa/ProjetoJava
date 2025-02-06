d9c31930e3158b194d76f58d1b8205b5a2670f98
antonio <antg3102@gmail.com>
2025-02-06 00:06:35 -0300

    Projeto Java

---
+++ src/ProjetoJava/arraysMultidimensionais02.java
@@ -0,0 +1,18 @@
+package ProjetoJava;
+
+public class arraysMultidimensionais02 {
+    public static void main(String[] args) {
+        int[][] arrayInt = new int[3][];
+
+        arrayInt[0] = new int[]{1,2};
+        arrayInt[1] = new int[]{1,2,3,4};
+        arrayInt[2] = new int[]{1,2,3,4,5,6,7};
+
+        for (int[] arrBase : arrayInt){
+            System.out.println("\n-------------");
+            for (int num : arrBase){
+                System.out.print(num + " ");
+            }
+        }
+    }
+}

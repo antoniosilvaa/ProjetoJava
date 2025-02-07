d113f64c8ec846cf58b2aed4c43d8fbd3fa19856
antonio <antg3102@gmail.com>
2025-02-07 00:13:21 -0300

    Projeto Java

--- src/introduçãoMetodos/Dominio/Calculadora.java
+++ src/introduçãoMetodos/Dominio/Calculadora.java
@@ -11,7 +11,14 @@ public class Calculadora {
         System.out.println(num1 * num2);
     }
     public int DivideDoisNumeros(int num1, int num2){
+        if (num2 == 0){
+            return 0;
+        } else if (num1 == 0) {
+            return 0;
+        } else {
+            return num1 / num2;
+        }
 
-        return num1 / num2;
+
     }
 }

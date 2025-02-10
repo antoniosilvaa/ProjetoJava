25ffa786b23e6e347ed777de028c7561b1d12234
antonio <antg3102@gmail.com>
2025-02-10 00:00:38 -0300

    Projeto Java

---
+++ src/Associação/Test/JogadorTest01.java
@@ -0,0 +1,18 @@
+package Associação.Test;
+
+import Associação.dominio.Jogador;
+
+import java.util.Scanner;
+
+public class JogadorTest01 {
+    public static void main(String[] args) {
+        Jogador jogador1 = new Jogador("romario");
+        Jogador jogador2 = new Jogador("Vina");
+        Jogador jogador3 = new Jogador("Pele");
+        Jogador[] jogadores = {jogador1,jogador2,jogador3};
+
+        for (Jogador jogador : jogadores){
+            jogador.imprime();
+        }
+    }
+}

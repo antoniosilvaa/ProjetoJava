027495f084e63d87d560e5dc09931aa4b4c43ff0
antonio <antg3102@gmail.com>
2025-02-16 23:03:12 -0300

    Projeto Java

---
+++ src/Herança/Test/HerançaTest01.java
@@ -0,0 +1,31 @@
+package Herança.Test;
+
+import Herança.dominio.Endereço;
+import Herança.dominio.Funcionario;
+import Herança.dominio.Pessoa;
+
+public class HerançaTest01 {
+    public static void main(String[] args) {
+        Endereço endereço = new Endereço();
+        Pessoa pessoa = new Pessoa();
+        endereço.setCep("60330-878");
+        endereço.setRua("Rua Irapuã");
+        endereço.getCep();
+        endereço.getRua();
+        pessoa.setNome("Antonio");
+        pessoa.setCpf("888888888888");
+        pessoa.setEndereço(endereço);
+        pessoa.imprime();
+
+        System.out.println("-----------");
+
+        Funcionario funcionario = new Funcionario();
+
+        funcionario.setNome("Bia");
+        funcionario.setCpf("99999999999");
+        funcionario.setSalario(2120);
+        funcionario.setEndereço(endereço);
+        funcionario.imprime();
+
+    }
+}
---
+++ src/Herança/dominio/Pessoa.java
@@ -0,0 +1,38 @@
+package Herança.dominio;
+
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private Endereço endereço;
+
+    public void imprime(){
+        System.out.println(this.endereço.getRua() + " " + this.endereço.getCep());
+        System.out.println(this.cpf);
+        System.out.println(this.nome);
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Endereço getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(Endereço endereço) {
+        this.endereço = endereço;
+    }
+}

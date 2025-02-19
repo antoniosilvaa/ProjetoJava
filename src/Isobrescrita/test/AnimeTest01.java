6dd1b893ace933c9ca9513f0d8e22be105b67a48
antonio <antg3102@gmail.com>
2025-02-19 15:27:45 -0300

    Projeto Java

---
+++ src/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package Isobrescrita.test;
+
+import Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("One piece");
+        System.out.println(anime.toString());
+    }
+}

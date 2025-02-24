72ce15c015e8b2f294e6bbe87be990c1e9e5e53e
antonio <antg3102@gmail.com>
2025-02-24 13:12:33 -0300

    Projeto Java

--- src/Polimorfismo/Test/ProdutoTest.java
+++ src/Polimorfismo/Test/ProdutoTest.java
@@ -5,7 +5,7 @@ import Polimorfismo.dominio.*;
 
 public class ProdutoTest {
     public static void main(String[] args) {
-        Computador computador = new Computador("Ryzer", 7000);
+        Computador computador = new Computador("Ryzen 9T", 7000);
         Televisao televisao = new Televisao("LG smart", 5340);
         Celular Iphone = new Celular("Iphone 15", 6000 );
         Tomate tomate = new Tomate("Americana", 20);

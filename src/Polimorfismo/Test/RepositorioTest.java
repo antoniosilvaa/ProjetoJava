59f55ba02e0852e8c509efa3631b1669ffe87e72
antonio <antg3102@gmail.com>
2025-02-27 00:42:00 -0300

    Atualização estudo de 300 dias Java 39/300

--- src/Polimorfismo/Test/RepositorioTest.java
+++ src/Polimorfismo/Test/RepositorioTest.java
@@ -11,9 +11,8 @@ public class RepositorioTest {
         Repositorio repositorio = new RepositorioBancoDeDados();
         repositorio.salvar();
         List <String> list = new ArrayList<>();
-        list.add("Antonio");
-        list.add("Beatriz");
-        list.add("55");
+        list.add("Antonio " + "85");
+        list.add("Beatriz " + "85");
         System.out.println(list);
     }
 }

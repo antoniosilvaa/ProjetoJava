11f75f94f7e115aaaf575c1542ba8502479970e1
antonio <antg3102@gmail.com>
2025-02-27 19:35:08 -0300

    Atualização estudo de 300 dias Java 39/300 entendendo sobre exepctions RunTime

---
+++ src/Excecoes/RunTime/Test/RunTimeExpectionTest.java
@@ -0,0 +1,9 @@
+package Excecoes.RunTime.Test;
+
+public class RunTimeExpectionTest {
+    public static void main(String[] args) {
+        int[] nums ={1,2};
+        System.out.println(nums[2]);
+        //como nao existe a posição numero 2 está dando a exeção RunTime
+    }
+}

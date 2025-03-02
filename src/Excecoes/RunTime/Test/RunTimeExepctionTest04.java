49fae5c6ec388d211c3399d5f643ff019a2fcaa4
antonio <antg3102@gmail.com>
2025-03-02 11:28:15 -0300

    Atualização estudo de 300 dias Java 42/300 entendendo sobre Mutiplas exeptions no RunTimeExepctionTest04.java

--- src/Excecoes/RunTime/Test/RunTimeExepctionTest04.java
+++ src/Excecoes/RunTime/Test/RunTimeExepctionTest04.java
@@ -6,15 +6,12 @@ import java.io.PrintStream;
 public class RunTimeExepctionTest04 {
     public static void main(String[] args) throws FileNotFoundException {
      try {
-         throw new ArithmeticException();
-     }catch (ArrayIndexOutOfBoundsException e){
-         e.printStackTrace(new PrintStream("falha no array"));
-     }catch (IllegalArgumentException e){
+         throw new ArrayIndexOutOfBoundsException();
+     }catch (IllegalArgumentException | ArithmeticException | ArrayIndexOutOfBoundsException e){
          //noinspection CallToPrintStackTrace
          e.printStackTrace();
-     }catch (ArithmeticException e){
-         //noinspection CallToPrintStackTrace
-         e.printStackTrace();
+     }catch (Exception e){
+         System.out.println("Dentro do RunTimeExeption");
      }
     }
 }

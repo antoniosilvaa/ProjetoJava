2a3506d1ca81c4ba838ba5885c8b6d12603beb47
antonio <antg3102@gmail.com>
2025-03-06 10:59:33 -0300

    Atualização estudo de 300 dias Java 45/300 entendendo sobre Classes Utilitarias

--- src/Wrappers/Test/wappersTest01.java
+++ src/Wrappers/Test/wappersTest01.java
@@ -2,6 +2,8 @@ package Wrappers.Test;
 
 public class wappersTest01 {
     public static void main(String[] args) {
+            Integer valor = Integer.parseInt("20");
+        System.out.println(valor);
 
     }
 }

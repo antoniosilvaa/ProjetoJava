1ed1ee5a7bbb7cf0a2f06932bba9eb8aefabcb59
antonio <antg3102@gmail.com>
2025-03-11 09:34:07 -0300

    Atualização estudo de 300 dias Java 48/300 entendendo sobre Classes Utilitarias

---
+++ src/Dates/test/DateTest01.java
@@ -0,0 +1,10 @@
+package Dates.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1741696391493L);
+        System.out.println(date);
+    }
+}

0128eee45c69c1f6ef1999e27bba4dab1f7856af
antonio <antg3102@gmail.com>
2025-03-23 23:19:48 -0300

    Formats 53/300

---
+++ src/Dates/test/InstantTest.java
@@ -0,0 +1,10 @@
+package Dates.test;
+
+import java.time.Instant;
+
+public class InstantTest {
+    public static void main(String[] args) {
+        Instant instant = Instant.now();
+        System.out.println(instant);
+    }
+}

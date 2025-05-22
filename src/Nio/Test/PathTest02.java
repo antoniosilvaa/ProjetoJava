0bbba006ee944c3ae1138b0b8fd680867a1fe3a5
antonio <antg3102@gmail.com>
2025-05-22 12:33:33 -0300

    Io Path | Paths | File | Files

--- src/Nio/Test/PathTest02.java
+++ src/Nio/Test/PathTest02.java
@@ -1,5 +1,6 @@
 package Nio.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,13 +18,14 @@ public class PathTest02 {
         Path subPastaPath = Paths.get("pasta/subpasta");
         Path subPastaPath2 = Paths.get("pasta2/subpasta");
         Path filePath = Paths.get(subPastaPath.toString(), "file.txt");
+        Path filePath2 = Paths.get(subPastaPath2.toString(), "file.txt2");
 
         if (Files.notExists(subPastaPath)) {
             Path pastaDirectories = Files.createDirectories(subPastaPath);
             System.out.println("Created Directories: " + pastaDirectories);
         } else if (Files.notExists(filePath)) {
           Path filePathIsCreated = Files.createFile(filePath);
-            System.out.println("Created File: " + filePath);
+            System.out.println("Created File: " + filePathIsCreated);
         } else {
             System.out.println("Subpastas ja criadas");
             System.out.println("File ja criada");
@@ -31,8 +33,12 @@ public class PathTest02 {
         if (Files.notExists(subPastaPath2)) {
             Path pastaDirectories2 = Files.createDirectories(subPastaPath2);
             System.out.println("Created Directories: " + pastaDirectories2);
-        }else {
+        } else if (Files.notExists(filePath2)) {
+            Path filePathIsCreated2 = Files.createFile(filePath2);
+            System.out.println("Created File: " + filePathIsCreated2);
+        } else {
             System.out.println("Subpastas ja criadas, agora na pasta2");
+            System.out.println("File2 ja criada");
         }
     }
 }

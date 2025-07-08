a9fc6923fc24dab18611361d988095ac047d4026
antonio <antg3102@gmail.com>
2025-07-08 12:20:33 -0300

    MethodReference2

--- src/Lambdas/test/MethodReferenceTest04.java
+++ src/Lambdas/test/MethodReferenceTest04.java
@@ -1,4 +1,19 @@
 package Lambdas.test;
 
+import Lambdas.Service.AnimeComparator;
+import Lambdas.domain.Anime;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class MethodReferenceTest04 {
+    public static void main(String[] args) {
+
+        Supplier<AnimeComparator> newAnimeComparators =AnimeComparator::new;
+        AnimeComparator animeComparators = newAnimeComparators.get();
+        AnimeComparator animeComparator = new AnimeComparator();
+        List<Anime> animeList =new ArrayList<>(List.of(new Anime("Naruto", 670), new Anime("pokemom", 671)));
+        animeList.sort(animeComparator::compareByEpisodesNonStatic);
+    }
 }

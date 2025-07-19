fd6ac6c43252a2cfd47f37e15de1c45e1a386869
antonio <antg3102@gmail.com>
2025-07-18 22:39:03 -0300

    MethodReferences

--- src/Lambdas/test/MethodReferenceTest04.java
+++ src/Lambdas/test/MethodReferenceTest04.java
@@ -14,6 +14,10 @@ public class MethodReferenceTest04 {
         AnimeComparator animeComparators = newAnimeComparators.get();
         AnimeComparator animeComparator = new AnimeComparator();
         List<Anime> animeList =new ArrayList<>(List.of(new Anime("Naruto", 670), new Anime("pokemom", 671)));
+        List<Anime> animeList2 =new ArrayList<>(List.of(new Anime("Naruto", 674), new Anime("pokemom", 675)));
         animeList.sort(animeComparator::compareByEpisodesNonStatic);
+        animeList2.sort(animeComparators::compareByEpisodesNonStatic);
+        System.out.println(animeList);
+        System.out.println(animeList2);
     }
 }

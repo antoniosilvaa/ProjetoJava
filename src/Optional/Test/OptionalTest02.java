effd5952c113117ea2e7f7399ba2ac2363275a1c
antonio <antg3102@gmail.com>
2025-07-19 15:15:56 -0300

    Novos Recursos em Optionals e Lambdas e Predicate

--- src/Optional/Test/OptionalTest02.java
+++ src/Optional/Test/OptionalTest02.java
@@ -1,4 +1,23 @@
 package Optional.Test;
 
+import Optional.Domain.Manga;
+import Optional.Repositorio.MangaRepository;
+
+import java.util.Optional;
+
 public class OptionalTest02 {
+    public static void main(String[] args) {
+      Optional<Manga> mangaByTitle = MangaRepository.findByTitle("Pokemom");
+                mangaByTitle.ifPresent(m-> m.setTitle("Pokemom 2"));
+        System.out.println(mangaByTitle);
+
+        Manga mangaById = MangaRepository.findById(2)
+                .orElseThrow(IllegalArgumentException::new);
+        System.out.println(mangaById);
+
+        Manga newManga = MangaRepository.findByTitle("Lego")
+                .orElse(new Manga(3, "lego", 234));
+
+        System.out.println(newManga);
+    }
 }
